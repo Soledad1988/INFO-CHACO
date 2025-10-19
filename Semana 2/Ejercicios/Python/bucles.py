54ca76710c0163b84a62b96c770b924b3eed4a4b
Brenda <brentorres26@gmail.com>
2025-10-19 17:33:09 -0300

    Ejercicio en Python

---
+++ Semana 2/Ejercicios/Python/bucles.py
@@ -0,0 +1,10 @@
+#While
+i = 1
+while i <=10:
+    print(i)
+    i+=1
+
+#For
+i = 1
+for i in range(1,11):
+    print(i)
---
+++ Semana 2/Ejercicios/Python/variables.py
@@ -0,0 +1,9 @@
+variable1 = 'Estos es un contenido de tipo str'
+variable2 = 1234567
+variable3 = 4.567
+variable4 = True
+
+print(type(variable1))
+print(type(variable2))
+print(type(variable3))
+print(type(variable4))
