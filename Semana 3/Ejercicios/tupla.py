629c8ec658339a95ea602dc422e86d81d6329e9d
Brenda <brentorres26@gmail.com>
2025-10-26 22:03:04 -0300

    Ejercicos - tuplas

---
+++ Semana 3/Ejercicios/tupla.py
@@ -0,0 +1,14 @@
+#Crear una tupla: crea una tupla my_tupla que contenga los números 1, 2, 3, 4 y 5.
+#Elementos de acceso: Acceda e imprima el tercer elemento de my_tuple.
+#Longitud de tupla: busque e imprima la longitud de my_tuple
+
+my_tupla = (1, 2, 3, 4 ,5)
+
+print('Mi tupla: ',my_tupla)
+
+print('El tercer elemento de mi_tupla:', my_tupla[2])
+
+print('La longitud de mi_tupla:', len(my_tupla))
+
+
+
